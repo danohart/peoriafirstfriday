09c3b861ec1db6f7a88321825ecbc6717041e548
Daniel Hart <me@danielhart.co>
2018-03-21 12:58:27 -0500

    Take out spaces after current_spacetype

--- themes/peoriafirstfriday-theme/archive-space.php
+++ themes/peoriafirstfriday-theme/archive-space.php
@@ -4,11 +4,11 @@
     	<div class="filter-cat">
     		Select: 
 		    <button class="all active">All</button>
-		    <button class="Studio" data-cat="Studio ">Studio</button>
-		    <button class="Gallery" data-cat="Gallery ">Gallery</button>
-		    <button class="Restuarant" data-cat="Restuarant ">Restuarant</button>
-		    <button class="Bar" data-cat="Bar ">Bar</button>
-		    <button class="Shop" data-cat="Shop ">Shop</button>
+		    <button class="Studio" data-cat="Studio">Studio</button>
+		    <button class="Gallery" data-cat="Gallery">Gallery</button>
+		    <button class="Restuarant" data-cat="Restuarant">Restuarant</button>
+		    <button class="Bar" data-cat="Bar">Bar</button>
+		    <button class="Shop" data-cat="Shop">Shop</button>
 		</div>
     	<div class="card-wrapper">
 	        <?php while ( have_posts() ) : the_post(); ?>
@@ -21,7 +21,7 @@
 	        $current_spacetypes = ( get_post_meta( $post->ID, '_space_spacetypes', true ) ) ? get_post_meta( $post->ID, '_space_spacetypes', true ) : array();
 
 	        ?>
-	      <div class="card" data-cat="<?php foreach ( $spacetypes as $spacetype ) {if (in_array($spacetype, $current_spacetypes)) { ?><?php echo $spacetype; ?> <?php }}?>">
+	      <div class="card"<?php foreach ( $spacetypes as $spacetype ) {if (in_array($spacetype, $current_spacetypes)) { ?> data-cat="<?php echo $spacetype; ?>"<?php }}?>>
 	      	<?php if ( has_post_thumbnail() ) { ?>
 				<div class="featured-image"><?php the_post_thumbnail('space');?></div>
 			<?php
@@ -43,8 +43,7 @@
 							if (in_array($spacetype, $current_spacetypes)) { ?>
 							    <input type="button" value="<?php echo $spacetype; ?>">
 							<?php }
-						}
-						?>
+						} ?>
 			    </div>
 	    	</div>
 	      </div>
